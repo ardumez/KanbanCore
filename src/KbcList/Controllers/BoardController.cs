0ee691574fa917c97bb994cf4441d4002a40451a
Arnauld DUMEZ <arnauld.dumez@live.com>
2016-12-07 17:19:41 +0100

    Changement

--- src/KbcList/Controllers/BoardController.cs
+++ src/KbcList/Controllers/BoardController.cs
@@ -1,17 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using KbcList.Models.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+
 
 namespace KbcList.Controllers
 {
     public class BoardController : Controller
     {
-        public IActionResult Index(int boardID)
+        private IDataContext dataContext;
+
+        public BoardController(IDataContext dataContext)
         {
-            if (IsSecure<UserBoard>())
+            this.dataContext = dataContext;
+        }
+
+        public IActionResult Index(int userID)
+        {
+      
             return View();
         }
 
--- src/KbcList/Models/Database/DataContext.cs
+++ src/KbcList/Models/Database/DataContext.cs
@@ -1,10 +1,27 @@
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+using KbcList.Models.BoardModels;
+using System.Linq;
 
 namespace KbcList.Models.Database
 {
+    public interface IDataContext
+    {
+        void AddEntity<TEntity>(TEntity entity) where TEntity : class;
+        IQueryable<TEntity> Query<TEntity>() where TEntity : class;
+    }
+
     public class DataContext : DbContext
     {
-         public DbSet<Blog> Blogs { get; set; }
+         public DbSet<Board> Boards { get; set; }
+
+         public void AddEntity<TEntity>(TEntity entity) where TEntity : class
+         {
+            base.Add<TEntity>(entity);
+         }
+
+         IQueryable<TEntity> Query<TEntity>() where TEntity : class
+         {
+             return base.Set<TEntity>();
+         }
     }
 }
