a95768b0ebea8a1ef9b5ad56ad778908f3429bea
Arnauld DUMEZ <arnauld.dumez@live.com>
2016-12-09 17:03:16 +0100

    Mise en angular

--- src/KbcList/wwwroot/js/controllers/board.js
+++ src/KbcList/wwwroot/js/controllers/board.js
@@ -1,24 +1,46 @@
 (function (angular) {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('KbcList')
-    .controller('BoardCtrl', BoardCtrl);
+    angular
+        .module('KbcList')
+        .controller('BoardCtrl', BoardCtrl)
+        .directive('boardList',BoardListDirective)
+        .directive('boardItem', BoardItemDirective);
 
-  BoardCtrl.$inject = ['$scope', 'kbcMenuService'];
+    BoardCtrl.$inject = ['$scope'];
 
-  function BoardCtrl($scope, kbcMenuService) {
-    var vm = this;
-    vm.message = "coucou";
-    vm.coucou = "coucouc";
-    vm.init = init;
-    return vm;
-
-    function init() {
-      kbcMenuService.getKBCMenu().success(function (data) {
-        vm.KbcList = data;
-      });
+    function BoardListDirective() {
+        return function (scope, element, attrs) {
+            if (scope.$last) { // all are rendered
+                element.bind("dragover", function(){
+                    console.log("click");
+                });
+            }
+        }
     }
-  };
+
+    function BoardItemDirective() {
+        return function (scope, element, attrs) {
+            if (scope.$last) { // all are rendered
+                element.bind("dragover", function(ev){
+                    console.log("click");
+                });
+            }
+        }
+    }
+
+    function BoardCtrl($scope, kbcMenuService) {
+        var vm = this;
+        vm.boardLists = [ {title : "A Faire", items : [
+                {title: "acheter baguette"},
+                {title: "acheter journal"}
+            ]
+        }, {title : "autre"}];
+        vm.init = init;
+        return vm;
+
+        function init() {
+        }
+    };
 
 })(window.angular);
